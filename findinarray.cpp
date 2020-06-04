8006f28ac158f122fc444b12790351381672594a
collinshayden <48135191+collinshayden@users.noreply.github.com>
2020-06-04 15:58:39 -0400

    Update findinarray.cpp

--- findinarray.cpp
+++ findinarray.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+  int num_to_find = 2;
+  int arr[5] = {5,4,1,2,6};
+  size_t arrsize = sizeof(arr) / sizeof(int);
+  int *end = arr + arrsize;
+  int *result = find(arr,end,num_to_find);
+  if (result == end){//if element is not in array. making this != will check if element is in array
+    cout<<"not in arr"<<endl;
+  }
+  else {
+    cout<<"in arr at index "; cout<<distance(arr,result)<<endl;//if element is in array
+  }
+
+}
