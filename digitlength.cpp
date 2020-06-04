2490c2c0b9bb8a43a493aa908dbeb23f2ff0ebf2
collinshayden <48135191+collinshayden@users.noreply.github.com>
2020-06-04 15:51:43 -0400

    comment

--- digitlength.cpp
+++ digitlength.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 using namespace std;
 
-int numLength(int n)
+int numLength(int n)//this takes n and returns the number of digits in n. 
 {
   int length;
   length = floor(log10(n)) + 1;
@@ -15,3 +15,4 @@ int main()
   cout<<"num: ";cout<<n;cout<<" length: ";cout<<numLength(n)<<endl;
 }
 
+//output: num: 795238 length: 6
