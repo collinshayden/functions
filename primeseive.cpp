90095383cd53c66a57b30e844cf8af92f5d9d70a
collinshayden <48135191+collinshayden@users.noreply.github.com>
2020-06-27 14:56:47 -0400

    return type is now a vector
    rather than simply printing the primes, now they are put into a vector and returned

--- primeseive.cpp
+++ primeseive.cpp
@@ -1,15 +1,16 @@
-//this will output all primes from 2 to n, defined in the function call
+//this will return a vector of all primes from 2 to n, where n is defined in the function call. 
 #include <iostream>
 #include <math.h>
+#include <vector>
 using namespace std;
 
-int seive(int n)
+std::vector<int> seive(int n)//sets return type to an integer vector
 {
   bool boolArr[n];//array of true/false. primes will be marked true
   size_t arrsize = sizeof(boolArr) / sizeof(bool); 
 
   for (int i = 0; i < arrsize; i++){
-    boolArr[i] = true;
+    boolArr[i] = true;//initializes all values as true
   } boolArr[0] = false; boolArr[1] = false;//sets 0 and 1 to false, since seive starts at 2
 
   for (int i = 2; i < sqrt(n)+1; i++){
@@ -19,14 +20,18 @@ int seive(int n)
       }
     }
   } 
+  vector<int> primeVector;//creates vector of integers
   for (int i = 0; i < arrsize; i++){
-    if (boolArr[i]){
-    cout<<"num: ";cout<<i;cout<<" bool: ";cout<<boolArr[i]<<endl;
+    if (boolArr[i]){//if index i of boolarr is true(prime)
+      primeVector.push_back(i);//add i to end of the vector
     }
-  }
+  } return primeVector;
 }
 
 int main()
 {
-  seive(20);
+  vector<int> vect = seive(1000);
+  for (int i: vect){//i is elements of vect
+    cout<<i<<endl;
+  }
 }
