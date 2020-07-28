b620caf3894a9a4e489c365fa1232de5f93aaccc
collinshayden <48135191+collinshayden@users.noreply.github.com>
2020-07-27 23:40:19 -0400

    25% more efficient

--- factor.cpp
+++ factor.cpp
@@ -5,8 +5,11 @@ using namespace std;
 
 std::vector<int> factor(int n) 
 {
-  vector<int> factorVect;
-  for (int i = 1; i < n/2 + 1; i++){
+  vector<int> factorVect; int step; 
+  if (n % 2 != 0){//odd numbers cannot have even divisors, so the step can be by 2 for odd numbers, making the algorithm 25% more efficient
+     step = 2;
+  }
+  for (int i = 1; i < n/2 + 1; i+= step){
     if (n % i == 0){
       factorVect.push_back(i);
     }
@@ -17,5 +20,5 @@ std::vector<int> factor(int n)
 
 int main()
 {
-  for (int i: factor(28)){cout<<i<<endl;}
+  for (int i: factor(11)){cout<<i<<endl;}
 }
